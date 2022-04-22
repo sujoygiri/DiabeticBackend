fdb98ee3bc89dd853753982f08677d4f746043d8
Sujoy Giri <girisujoy630@gmail.com>
2022-04-22 20:22:30 +0530

    updating prediction view

--- DiabaticPrediction/models.py
+++ DiabaticPrediction/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class PredictionValue(models.Model):
-    glucose = models.IntegerField(blank=False, null=False, max_length=4)
-    blood_pressure = models.IntegerField(blank=False, null=False, max_length=4)
-    skin_thickness = models.IntegerField(blank=False, null=False, max_length=4)
-    insulin = models.IntegerField(blank=False, null=False, max_length=4)
-    bmi = models.FloatField(blank=False, null=False, max_length=4)
-    age = models.IntegerField(blank=False, null=False, max_length=3)
+    glucose = models.IntegerField(blank=False, null=False)
+    blood_pressure = models.IntegerField(blank=False, null=False)
+    skin_thickness = models.IntegerField(blank=False, null=False)
+    insulin = models.IntegerField(blank=False, null=False)
+    bmi = models.FloatField(blank=False, null=False)
+    age = models.IntegerField(blank=False, null=False)
     
--- DiabaticPrediction/views.py
+++ DiabaticPrediction/views.py
@@ -1,18 +1,34 @@
-from django.shortcuts import render
+from .serializer import PredictionValueSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# from pathlib import Path
-# import joblib
-# import numpy as np
+from pathlib import Path
+import joblib
+import numpy as np
 
-# script_loacation = Path(__file__).absolute().parent
-# model_location = script_loacation / './assets/Diabetes_prediction_ml_model.cls'
+script_loacation = Path(__file__).absolute().parent
+model_location = script_loacation / './assets/Diabetes_prediction_ml_model.cls'
 
 # model = joblib.load(model_location)
-
 # user_data = np.asarray((149,68,29,127,29.3,22))
 # reshaped_user_data = user_data.reshape(1,-1)
 # print(model.predict(reshaped_user_data))
 
-
+@api_view(['POST'])
 def prediction(request):
-    pass
+    if request.method == 'POST':
+        serializer = PredictionValueSerializer(data=request.data)
+        if serializer.is_valid():
+            glucose = serializer.data.get('glucose')
+            blood_pressure = serializer.data.get('blood_pressure')
+            skin_thickness = serializer.data.get('skin_thickness')
+            insulin = serializer.data.get('insulin')
+            bmi = serializer.data.get('bmi')
+            age = serializer.data.get('age')
+            user_data = np.asarray((glucose,blood_pressure,skin_thickness,insulin,bmi,age))
+            reshaped_user_data = user_data.reshape(1,-1)
+            model = joblib.load(model_location)
+            result = model.predict(reshaped_user_data)
+            return Response(result, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
